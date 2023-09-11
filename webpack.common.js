9843dace9890a5fdd0e54f67133ee1de7985c7ae
Edun Omatseye <edunomatseye@yahoo.com>
2023-09-11 17:29:47 +0100

    extended webpack config file to accept more enviroment file

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    "resolve": {
+        "extensions": [".tsx", ".ts", ".jsx", ".js"],
+    },
+    "entry": path.resolve(__dirname, "./src/main.tsx"),
+    "output": {
+        path: path.resolve(__dirname, "./build"),
+        "filename": "bundle.js"
+    },
+    "module": {
+        "rules": [
+            {
+                test: /\.(ts|js)x?$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    }
+                ],
+                exclude: /node_modules/
+            },{
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            
+            },{
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource'
+            },{
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "./src/index.html"),
+        })
+    ]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,11 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge')
 
-module.exports = {
-    "resolve": {
-        "extensions": [".tsx", ".ts", ".jsx", ".js"],
-    },
-    "entry": path.resolve(__dirname, "./src/main.tsx"),
-    "output": {
-        path: path.resolve(__dirname, "./build"),
-        "filename": "bundle.js"
-    },
-    "mode": "development",
-    "module": {
-        "rules": [
-            {
-                test: /\.(ts|js)x?$/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                    }
-                ],
-                exclude: /node_modules/
-            },{
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            
-            },{
-                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-                type: 'asset/resource'
-            },{
-                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-                type: 'asset/inline'
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "./src/index.html"),
-        })
-    ]
+module.exports = function(envVars) {
+    const { env } = envVars;
+    const dev = require('./webpack.dev')
+    const prod = require('./webpack.prod')
+    const common = require('./webpack.common')
+
+    const config = merge(common, require(`./webpack.${env}`))
+    return config
 }
