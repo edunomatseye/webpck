d77dec72e7cb52ab7ced2cc3122165ec881f791f
Edun Omatseye <edunomatseye@yahoo.com>
2023-10-02 16:53:37 +0100

    chore: added zod and localStorage api

--- localstorage/src/lib/localstorage.ts
+++ localstorage/src/lib/localstorage.ts
@@ -1,3 +1,36 @@
+import z from 'zod'
+
 export function localstorage(): string {
-    return 'localstorage';
+    return 'localstorage'
+}
+
+const localStorage = new Map()
+
+const objectSchema = z.object({
+    simpleToken: z.string().optional(),
+    resetToken: z.string().optional(),
+    complexToken: stringifyToken({
+        iss: z.string().uuid(),
+    }),
+})
+
+type KeyValueMap = z.infer<typeof objectSchema>
+type Key = keyof KeyValueMap
+type Value<K extends Key> = KeyValueMap[K]
+
+export const getLocalStorageItem = <K extends Key>(key: K) => {
+    localStorage.get(key)
+}
+
+export const setLocalStorageItem = <K extends Key, V extends Value<K>>(
+    key: K,
+    value: V
+) => {
+    localStorage.set(key, value)
+}
+
+function stringifyToken(ObjectSchema: z.AnyZodObject) {
+    return z.string().transform((arg) => {
+        ObjectSchema.parse(arg)
+    })
 }
