574e758efef10fbef99e0fabdd077d98af938167
Edun Omatseye <edunomatseye@yahoo.com>
2023-10-04 17:40:23 +0100

    fix: zod record type

--- src/library/localstorage/src/lib/localstorage.ts
+++ src/library/localstorage/src/lib/localstorage.ts
@@ -9,9 +9,11 @@ const localStorage = new Map()
 const objectSchema = z.object({
     simpleToken: z.string().optional(),
     resetToken: z.string().optional(),
-    complexToken: stringifyToken({
-        iss: z.string().uuid(),
-    }),
+    complexToken: stringifyToken(
+        z.object({
+            iss: z.string().uuid(),
+        })
+    ),
 })
 
 type KeyValueMap = z.infer<typeof objectSchema>
@@ -29,8 +31,8 @@ export const setLocalStorageItem = <K extends Key, V extends Value<K>>(
     localStorage.set(key, value)
 }
 
-function stringifyToken(ObjectSchema: z.AnyZodObject) {
+function stringifyToken(ObjectSchema: z.ZodSchema) {
     return z.string().transform((arg) => {
-        ObjectSchema.parse(arg)
+        ObjectSchema?.parse(arg)
     })
 }
