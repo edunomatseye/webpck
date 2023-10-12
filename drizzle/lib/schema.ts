425bcc991b9757d2bfc5816d2a4f80b3d4370546
Edun Omatseye <edunomatseye@yahoo.com>
2023-10-12 19:53:46 +0100

    chore: added first migration and update driver property of schema.ts

--- drizzle/lib/schema.ts
+++ drizzle/lib/schema.ts
@@ -3,11 +3,37 @@ import type { BetterSQLite3Database } from 'drizzle-orm/better-sqlite3'
 
 import Database from 'better-sqlite3'
 
-import { sqliteTable, integer } from 'drizzle-orm/sqlite-core'
+import {
+    sqliteTable,
+    integer,
+    uniqueIndex,
+    text,
+} from 'drizzle-orm/sqlite-core'
 
 const sqlite = new Database('sqite.db')
 const db: BetterSQLite3Database = drizzle(sqlite)
 
-const employee = sqliteTable('employee', {
+export const employee = sqliteTable('employee', {
     id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+})
+
+export const countries = sqliteTable(
+    'countries',
+    {
+        id: integer('id', { mode: 'number' }).primaryKey({
+            autoIncrement: true,
+        }),
+        name: text('name'),
+    },
+    (countries) => {
+        return {
+            nameIdx: uniqueIndex('nameIdx').on(countries.name),
+        }
+    }
+)
+
+export const cities = sqliteTable('cities', {
+    id: integer('id').primaryKey(),
+    name: text('name'),
+    countryId: integer('country_id').references(() => countries.id),
 })
---
+++ drizzle/src/0000_peaceful_enchantress.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `cities` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`name` text,
+	`country_id` integer,
+	FOREIGN KEY (`country_id`) REFERENCES `countries`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `countries` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text
+);
+--> statement-breakpoint
+CREATE TABLE `employee` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `nameIdx` ON `countries` (`name`);
